d8863d0028c7e2517ec1defb49930516236b8197
RiccardoGrande <grandericcardo1997@gmail.com>
2023-05-22 16:30:07 +0200

    add second page

--- laravel-primi-passi/resources/views/about.blade.php
+++ laravel-primi-passi/resources/views/about.blade.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
-<h1>Hello world</h1>
 
+<h1>What's poppin? Why are you here?</h1>
 <a href="/">Home</a>
-<a href="/about">About</a>
 </body>
 </html>
